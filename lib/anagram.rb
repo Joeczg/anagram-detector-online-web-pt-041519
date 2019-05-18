a6c0167496dcb6f31fc8cb4fae6021aa9e237be0
zhenggao cai <caizhengao1992@gmail.com>
2019-05-18 06:04:03 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,13 @@
 # Your code goes here!
+class Anagram
+  attr_accessor :word 
+  def initialize(word)
+    @word = word
+  end
+  def match(anagrams)
+    @anagrams = anagrams
+    array = []
+    array = anagrams.split.find {|items| items.sort == @word}
+    
+  end
+end
