19021a68820736c7893b0cd008d4b98922c0e177
zhenggao cai <caizhengao1992@gmail.com>
2019-05-18 06:09:04 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -7,7 +7,7 @@ class Anagram
   def match(anagrams)
     @anagrams = anagrams
     anagrams.select do |items|
-      items.split(" ").sort! == @word.sort!
+      items.split(" ").sort == @word.sort
     end
     
     
